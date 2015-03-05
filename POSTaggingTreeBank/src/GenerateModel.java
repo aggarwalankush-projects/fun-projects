384f26098bd476e7133b4b1e21f55ad37ffc9bf6
Ankush <Anku@172.24.16.43>
2015-03-05 16:16:57 -0500

    first commit

---
+++ POSTaggingTreeBank/src/GenerateModel.java
@@ -0,0 +1,97 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.postag.WordTagSampleStream;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.model.ModelType;
+
+public class GenerateModel {
+	String file = "treebank-sentences.txt";
+	String trainFile = "training.train";
+	String testFile = "testing.test";
+	String modelFile = "firstModel.bin";
+
+	public void divideFile() throws Exception {
+		BufferedReader brFile = new BufferedReader(new FileReader(file));
+		BufferedWriter bwTrain = new BufferedWriter(new FileWriter(trainFile));
+		BufferedWriter bwTest = new BufferedWriter(new FileWriter(testFile));
+
+		String line = null;
+		int j = 0;
+
+		while ((line = brFile.readLine()) != null) {
+			StringBuilder newLine = new StringBuilder();
+			j++;
+			String[] str = line.split("\t");
+			String[] ss = str[1].split(" ");
+			for (String s : ss) {
+				String[] word = s.split("/");
+				String tag = word[word.length - 1];
+				for (int i = 0; i < word.length - 1; i++)
+					newLine.append(word[i] + "/");
+				newLine.deleteCharAt(newLine.length() - 1);
+				newLine.append("_" + tag + " ");
+			}
+
+			if (j <= 500) {
+				bwTrain.write(newLine.toString() + "\n");
+			} else {
+				bwTest.write(newLine.toString() + "\n");
+			}
+		}
+		brFile.close();
+		bwTest.close();
+		bwTrain.close();
+
+		System.out.println("done generating testing and training files");
+
+	}
+
+	@SuppressWarnings("deprecation")
+	public void generateModel() throws Exception {
+
+		POSModel model = null;
+
+		InputStream dataIn = null;
+
+		dataIn = new FileInputStream(trainFile);
+		ObjectStream<String> lineStream = new PlainTextByLineStream(dataIn,
+				"UTF-8");
+		ObjectStream<POSSample> sampleStream = new WordTagSampleStream(
+				lineStream);
+
+		model = POSTaggerME.train("en", sampleStream, ModelType.MAXENT, null,
+				null, 0, 5);
+
+		OutputStream modelOut = null;
+
+		modelOut = new BufferedOutputStream(new FileOutputStream(modelFile));
+		model.serialize(modelOut);
+
+		System.out.println("model generation done");
+	}
+
+	public static void main(String[] args) {
+
+		GenerateModel gm = new GenerateModel();
+
+		try {
+			gm.divideFile();
+			gm.generateModel();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ POSTaggingTreeBank/src/GenerateTagsTwoB.java
@@ -0,0 +1,207 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+
+public class GenerateTagsTwoB {
+
+	String secondModel = "secondModel.bin";
+	String testFile = "testingB.test";
+	String predictions = "part-B-predictions.tsv";
+
+	HashSet<String> snn = new HashSet<String>();
+	HashSet<String> svb = new HashSet<String>();
+	HashSet<String> sjj = new HashSet<String>();
+	HashSet<String> srb = new HashSet<String>();
+
+	public GenerateTagsTwoB() {
+		snn.add("NN");
+		snn.add("NNS");
+		snn.add("NNP");
+		snn.add("NNPS");
+		snn.add("PRP");
+		snn.add("PRP$");
+		svb.add("VB");
+		svb.add("VBP");
+		svb.add("VBD");
+		svb.add("VBN");
+		svb.add("VBZ");
+		svb.add("VBG");
+		sjj.add("JJ");
+		sjj.add("JJR");
+		sjj.add("JJS");
+		srb.add("RB");
+		srb.add("RBR");
+		srb.add("RBS");
+	}
+
+	public String doMapping(String mapMe) {
+
+		if (snn.contains(mapMe))
+			return "SNN";
+		else if (svb.contains(mapMe))
+			return "SVB";
+		else if (sjj.contains(mapMe))
+			return "SJJ";
+		else if (srb.contains(mapMe))
+			return "SRB";
+		else
+			return "misc";
+
+	}
+
+	@SuppressWarnings("deprecation")
+	public void doAllWork() throws Exception {
+
+		InputStream modelIn = null;
+		POSModel model = null;
+
+		modelIn = new FileInputStream(secondModel);
+		model = new POSModel(modelIn);
+
+		POSTaggerME tagger = new POSTaggerME(model);
+		BufferedReader br = new BufferedReader(new FileReader(testFile));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(predictions));
+		String line = new String();
+		StringBuilder newTag = new StringBuilder();
+		StringBuilder oldTag = new StringBuilder();
+		int id = 500;
+		while ((line = br.readLine()) != null) {
+			String[] sent = line.split(" ");
+			StringBuilder tagline = new StringBuilder();
+			for (String s : sent) {
+				String words[] = s.split("_");
+				String tag = tagger.tag(words[0]);
+
+				String[] tt = tag.split("/");
+
+				newTag.append(tt[tt.length - 1] + " ");
+
+				oldTag.append(words[1] + " ");
+				tagline.append(tag + " ");
+			}
+			bw.write(id + "\t" + line.replace('_', '/') + "\t" + tagline + "\n");
+
+			id++;
+
+		}
+
+		String[] oldTags = oldTag.toString().split(" ");
+		String[] newTags = newTag.toString().split(" ");
+
+		int counter = 0;
+		int len = newTags.length;
+
+		HashSet<String> uniqueTags = new HashSet<String>();
+		HashSet<String> uniqueTagsNew = new HashSet<String>();
+		for (int i = 0; i < len; i++) {
+			uniqueTags.add(oldTags[i]);
+			uniqueTagsNew.add(newTags[i]);
+			if (newTags[i].equals(oldTags[i])) {
+				counter++;
+			}
+		}
+		System.out.println(uniqueTags.size());
+		System.out.println(uniqueTagsNew.size());
+		int ir = 0;
+		for (String s : uniqueTags) {
+			if (uniqueTagsNew.contains(s))
+				ir++;
+			else
+				System.out.print(s + " ");
+		}
+		System.out.println("\n" + ir);
+		ir = 0;
+		for (String s : uniqueTagsNew) {
+			if (uniqueTags.contains(s))
+				ir++;
+			else
+				System.out.print(s + " ");
+		}
+		System.out.println("\n" + ir);
+		System.out.println(len);
+		double totalAccuracy = (double) counter / len * 100;
+		System.out.println("Accuracy : " + (double) counter / len * 100);
+
+		int tagsCount = uniqueTags.size();
+
+		int[][] confusionMatrix = new int[tagsCount][tagsCount];
+
+		String[] alltags = new String[tagsCount];
+
+		alltags = uniqueTags.toArray(alltags);
+
+		for (int l = 0; l < tagsCount; l++) {
+			String tag = alltags[l];
+			for (int i = 0; i < len; i++) {
+				if (oldTags[i].equals(tag)) {
+					if (oldTags[i].equals(newTags[i]))
+						confusionMatrix[l][l]++;
+					else {
+						for (int q = 0; q < alltags.length; q++)
+							if (alltags[q].equals(newTags[i]))
+								confusionMatrix[l][q]++;
+					}
+				}
+			}
+		}
+		System.out.println(Arrays.toString(alltags));
+
+		BufferedWriter bwr = new BufferedWriter(new FileWriter(
+				"confusionMatrixTwoB.tsv"));
+		for (String s : alltags)
+			bwr.write("\t" + s);
+		bwr.write("\n");
+		for (int i = 0; i < tagsCount; i++) {
+			bwr.write(alltags[i]);
+
+			for (int j = 0; j < tagsCount; j++)
+				bwr.write("\t" + confusionMatrix[i][j]);
+
+			bwr.write("\n");
+		}
+
+		double perAcc, sum = 0;
+		BufferedWriter bwrr = new BufferedWriter(new FileWriter(
+				"perTagAccuracyTwoB.tsv"));
+		bwrr.write("Original POS Category\tAccuracy\n");
+		for (int i = 0; i < tagsCount; i++) {
+			sum = 0;
+			bwrr.write(alltags[i] + "\t");
+
+			for (int j = 0; j < tagsCount; j++) {
+				sum += confusionMatrix[i][j];
+			}
+			perAcc = confusionMatrix[i][i] / sum * 100;
+			bwrr.write("" + Math.round(perAcc * 100.0) / 100.0);
+			bwrr.write("\n");
+		}
+		bwrr.write("Overall Accuracy\t" + Math.round(totalAccuracy * 100.0)
+				/ 100.0);
+		bwrr.close();
+		bwr.close();
+		br.close();
+		bw.close();
+
+	}
+
+	public static void main(String[] args) {
+		GenerateTagsTwoB generateTags = new GenerateTagsTwoB();
+		try {
+			generateTags.doAllWork();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
